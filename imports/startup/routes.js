15e8af1a8f1bca2cd6880e293edefd545e6a8fd8
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2016-05-16 19:14:46 -0400

    Add layout template with navbar

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -2,6 +2,10 @@ import { Router } from 'meteor/iron:router';
 
 import '../ui/body.js';
 
+Router.configure({
+  layoutTemplate: 'main'
+});
+
 Router.route('/', {
   name: 'home',
   template: 'home'
